1795675be1df3b4fe01c7fd4caed2f07418fdfdb
Carles Furriols <carlesfurriols@gmail.com>
2025-03-24 14:56:06 +0100

    Seguim Mon Mar 24 14:55

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 public class Main{
 
     public static void menuPrincipal(){
-
-    }
-    public static void gestioDeProductes(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. Gestió de productes");
-        System.out.println("2. Gestió de compres i vendes");
-        System.out.println("9. Acabar");
+        //Per llegir l'entrada de l'usuari
+
+        //Mostra les opcions del Menú Principal
+        System.out.println("Benvingut al Menú Principal"); //Salutació
+        System.out.println("1. Gestió de productes"); // Per navegar al menú gestió de productes
+        System.out.println("2. Gestió de compres i vendes"); // Per navegar al menú de compres i vendes
+        System.out.println("9. Acabar"); // Per acabar el programa
 
         int navegacio = scanner.nextInt();
-        switch(navegacio){
+        //Navegació del menú
+        switch (navegacio) {
             case 1:
                 menuDeGestioDeProductes();
                 break;
@@ -19,27 +21,44 @@ public class Main{
                 menuDeGestioDeCompresIVendes();
                 break;
             case 9:
-                acabar();
+                acabarPrograma();
+                break;
         }
-    }
-    public static void gestioDeCompresIVendes(){
-
     }
 
     //MENÚ DE GESTIÓ DE PRODUCTES///
     public static void menuDeGestioDeProductes(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Benvingut al Menú de Gestió de Productes");
+        System.out.println("Introdueix el número per dir on vols navegar: ");
+        System.out.println("1. Afegir");
+        System.out.println("2. Eliminar");
+        System.out.println("3. Llistar productes");
+        System.out.println("9. Tornar");
+        int navegacio = scanner.nextInt();
+        switch(navegacio){
+            case 1:
+                afegirProducte();
+                break;
+            case 2:
+                eliminarProducte();
+                break;
+            case 3:
+                llistarProductes();
+                break;
+            case 9:
+                tornar();
+                break;
+        }
+    }
+    public static void afegirProducte(){
 
     }
-    public static void afegir(){
-
-    }
-    public static void eliminar(){
+    public static void eliminarProducte(){
 
     }
     public static void llistarProductes(){
-
-    }
-    public static void tornar(){
 
     }
 
@@ -47,46 +66,31 @@ public class Main{
     public static void menuDeGestioDeCompresIVendes(){
 
     }
-    public static void compra(){
+    public static void compraProducte(){
 
     }
-    public static void venda(){
+    public static void vendaProducte(){
 
     }
     public static void llistarEstocs(){
 
     }
-    public static void acabar(){
 
+    // FUNCIONALITATS //
+
+    public static void tornar(){
+        menuPrincipal();
+    }
+    public static void acabarPrograma(){
+        System.out.println("Adéu Siau, quan tornis farem negocis!");
+        return;
     }
     public static void main (String[] args){
 
         //MENÚ PRINCIPAL///
         menuPrincipal();
-        gestioDeProductes();
-        gestioDeCompresIVendes();
-
-        //MENÚ DE GESTIÓ DE PRODCUTES///
-        menuDeGestioDeProductes();
-        afegir();
-        eliminar();
-        llistarProductes();
-        tornar();
-
-        //MENÚ DE GESTIÓ DE COMPRES I VENDES///
-        menuDeGestioDeCompresIVendes();
-        compra();
-        venda();
-        llistarEstocs();
-        tornar();
-
-
-        // MOC acabar(); per fer una sortida més simple
-        acabar();
-
 
     }
-
 }
 
 
