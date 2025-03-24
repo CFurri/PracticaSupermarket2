a151269016177eee600a650af04f6b3e8906c8e9
Carles Furriols <carlesfurriols@gmail.com>
2025-03-24 23:45:19 +0100

    Seguim Mon Mar 24 14:55

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class Main{
 //comprovació commit
-    public static void menuPrincipal(){
-        Scanner scanner = new Scanner(System.in);
-        //Per llegir l'entrada de l'usuari
+    public static void menuPrincipal(String[] productes, int[] estocs){
+        Scanner scanner = new Scanner(System.in); //Per llegir l'entrada de l'usuari
 
         //Mostra les opcions del Menú Principal
         System.out.println("Benvingut al Menú Principal"); //Salutació
@@ -15,23 +14,26 @@ public class Main{
         //Navegació del menú
         switch (navegacio) {
             case 1:
-                menuDeGestioDeProductes();
+                menuDeGestioDeProductes(productes, estocs);
                 break;
             case 2:
-                menuDeGestioDeCompresIVendes();
+                menuDeGestioDeCompresIVendes(productes, estocs);
                 break;
             case 9:
                 acabarPrograma();
+                break;
+            default:
+                System.out.println("Opció incorrecta. Torna-ho a provar.");
+                menuPrincipal(productes, estocs);
                 break;
         }
     }
 
     //MENÚ DE GESTIÓ DE PRODUCTES///
-    public static void menuDeGestioDeProductes(){
+    public static void menuDeGestioDeProductes(String[] productes, int[] estocs){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Benvingut al Menú de Gestió de Productes");
-        System.out.println("Introdueix el número per dir on vols navegar: ");
         System.out.println("1. Afegir");
         System.out.println("2. Eliminar");
         System.out.println("3. Llistar productes");
@@ -39,7 +41,7 @@ public class Main{
         int navegacio = scanner.nextInt();
         switch(navegacio){
             case 1:
-                afegirProducte();
+                afegirProducte(productes, estocs);
                 break;
             case 2:
                 eliminarProducte();
@@ -48,11 +50,19 @@ public class Main{
                 llistarProductes();
                 break;
             case 9:
-                tornar();
+                tornar(productes, estocs);
+                break;
+            default:
+                System.out.println("Opció incorrecta. Torna-ho a provar.");
+                menuDeGestioDeProductes(productes, estocs);
                 break;
         }
     }
-    public static void afegirProducte(){
+    public static void afegirProducte(String[] productes, int[] estoc){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nom del producte: ");
+        String nomNouProducte = scanner.nextLine();
 
     }
     public static void eliminarProducte(){
@@ -63,10 +73,35 @@ public class Main{
     }
 
     //MENÚ DE GESTIÓ DE COMPRES I VENDES///
-    public static void menuDeGestioDeCompresIVendes(){
+    public static void menuDeGestioDeCompresIVendes(String[] productes, int[] estocs){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Benvingut al Menú de Gestió de Compres i Vendes");
+        System.out.println("1. Compra");
+        System.out.println("2. Venda");
+        System.out.println("3. Llistar estocs");
+        System.out.println("9. Tornar");
+        int navegacio = scanner.nextInt();
+        switch(navegacio){
+            case 1:
+                comprarProducte();
+                break;
+            case 2:
+                vendaProducte();
+                break;
+            case 3:
+                llistarEstocs();
+                break;
+            case 9:
+                tornar(productes, estocs);
+                break;
+            default:
+                System.out.println("Opció incorrecta. Torna-ho a provar.");
+                menuDeGestioDeCompresIVendes(productes, estocs);
+                break;
+        }
     }
-    public static void compraProducte(){
+    public static void comprarProducte(){
 
     }
     public static void vendaProducte(){
@@ -78,17 +113,19 @@ public class Main{
 
     // FUNCIONALITATS //
 
-    public static void tornar(){
-        menuPrincipal();
+    public static void tornar(String[] productes, int[] estocs){
+        menuPrincipal(productes, estocs);
     }
     public static void acabarPrograma(){
         System.out.println("Adéu Siau, quan tornis farem negocis!");
-        return;
     }
-    public static void main (String[] args){
 
-        //MENÚ PRINCIPAL///
-        menuPrincipal();
+    // MAIN //
+    public static void main (String[] args){
+        String[] productes = new String[100];
+        int[] estocs = new int[100];
+        //NAVEGACIÓ MENÚ PRINCIPAL///
+        menuPrincipal(productes, estocs);
 
     }
 }
